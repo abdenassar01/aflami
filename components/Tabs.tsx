a6237065aa5e606b06604c1945f4b1bbba71de5f
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-26 15:27:41 +0100

    adding icons to bottom-tab

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -3,17 +3,17 @@ import Movies from './screens/movies/Movies';
 import Series from './screens/series/Series';
 import { Image } from 'react-native';
 
+const Tab = createBottomTabNavigator();
+
+
 const Tabs = () => {
-
-    const Tab = createBottomTabNavigator();
-
   return (
     <Tab.Navigator initialRouteName='Movies' screenOptions={{ headerShown: false }}>
       <Tab.Screen name='Movies' component={ Movies } options={{
-        tabBarIcon: () => (<Image source={require("../asset/movie.png")} style={{width: 30, height: 30}} />)
+        tabBarIcon: () => (<Image source={ require("../asset/movie.png")} style={{width: 30, height: 30}} />)
       }}/>
       <Tab.Screen name='Series' component={ Series } options={{
-        tabBarIcon: () => (<Image source={require("../asset/movie.png")} style={{width: 30, height: 30}} />)
+        tabBarIcon: () => (<Image source={ require("../asset/film.png")} style={{width: 30, height: 30}} />)
       }}/>
     </Tab.Navigator>
   )
--- components/screens/movies/Movies.tsx
+++ components/screens/movies/Movies.tsx
@@ -4,9 +4,6 @@ export default function Movies() {
   return (
     <View>
       <Text>This is a Movies screen</Text>
-      <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam quod possimus tempora in quaerat est molestiae asperiores! Tempore ducimus quis corrupti eligendi autem eos laboriosam eum minus? Itaque, quia natus?</Text>
-      <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam quod possimus tempora in quaerat est molestiae asperiores! Tempore ducimus quis corrupti eligendi autem eos laboriosam eum minus? Itaque, quia natus?</Text>
-      <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam quod possimus tempora in quaerat est molestiae asperiores! Tempore ducimus quis corrupti eligendi autem eos laboriosam eum minus? Itaque, quia natus?</Text>
     </View>
   )
 }

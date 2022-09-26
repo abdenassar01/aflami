d5e02411ce2b7720957e417a8aecac38e2e92a3e
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-26 23:33:01 +0100

    finishing the layout of movie details page

--- components/screens/movie/movieDetails/MovieDetails.tsx
+++ components/screens/movie/movieDetails/MovieDetails.tsx
@@ -1,4 +1,4 @@
-import { ButtonText, GoBackBtn, MovieDetailsWrapper } from "./styles/Styles";
+import { ButtonText, DetailsWrapper, GoBackBtn, Heading, Info, MovieDetailsWrapper, Qualities, QualityItem, QualityItemText, SmallHeading, Streamer, Title } from "./styles/Styles";
 
 export default function MovieDetails({ navigation }) {
   return (
@@ -6,6 +6,25 @@ export default function MovieDetails({ navigation }) {
       <GoBackBtn onPress={ () => navigation.goBack() }>
         <ButtonText>Go Back</ButtonText>
       </GoBackBtn>
+      <DetailsWrapper>
+        <Streamer source={{ uri: "https://sydneymillage.files.wordpress.com/2021/05/bear-cross.jpg?w=301" }} />
+        <Title>A Cross To Bear (2012)</Title>
+        <Info>
+          <SmallHeading>Rating: {6.8}</SmallHeading>
+          <SmallHeading>Year: {2012}</SmallHeading>
+        </Info>
+        <Heading>Available Quality:</Heading>
+        <Qualities horizontal={true}>
+          <QualityItem onPress={ () => console.log("I'm Pressed") }><QualityItemText>1080p</QualityItemText></QualityItem>
+          <QualityItem onPress={ () => console.log("I'm Pressed") }><QualityItemText>720p</QualityItemText></QualityItem>
+          <QualityItem onPress={ () => console.log("I'm Pressed") }><QualityItemText>480p</QualityItemText></QualityItem>
+          <QualityItem onPress={ () => console.log("I'm Pressed") }><QualityItemText>360p</QualityItemText></QualityItem>
+        </Qualities>
+        <Heading>Discription: </Heading>
+        <SmallHeading>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi voluptate, perferendis dolor recusandae debitis illo voluptatibus ratione quasi sint qui tempore inventore sit esse quo rem! Cupiditate unde quibusdam obcaecati.
+        </SmallHeading>
+      </DetailsWrapper>
     </MovieDetailsWrapper>
   )
 }
--- components/screens/movie/movieDetails/styles/Styles.tsx
+++ components/screens/movie/movieDetails/styles/Styles.tsx
@@ -1,16 +1,68 @@
-import { Pressable, View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
 import styled from "styled-components";
+import { Color } from "../../../../utils/color/colors";
 
 export const MovieDetailsWrapper = styled(View)`
     background-color: white;
+    min-height: 100%;
 `
 
-export const GoBackBtn = styled(Pressable)`
-    padding: 5px;
+export const GoBackBtn = styled(TouchableOpacity)`
+    padding: 10px;
 `
 
 export const ButtonText = styled(Text)`
-    color: #165ffd;
+    color: #7ea6fc;
     font-weight: bold;
     font-size: 16px;
+`
+
+export const DetailsWrapper = styled(View)`
+    display: flex;
+    padding: 0 10px;
+`
+
+export const Streamer = styled(Image)`
+    width: 100%;
+    height: 220px;
+    border-radius: 10px;
+`
+
+export const Title = styled(Text)`
+    font-size: 18px;
+    margin: 5px 0;
+    font-weight: bold;
+    color: black;
+`
+
+export const Info = styled(View)`
+    margin: 5px 0;
+`
+
+export const SmallHeading = styled(Text)`
+    font-size: 12px;
+    color: #070606;
+`
+
+export const Heading = styled(Text)`
+    margin: 5px 0;
+    font-size: 14px;
+    color: black;
+    font-weight: 500;
+`
+
+export const Qualities = styled(ScrollView)`
+    margin: 10px 0;
+`
+
+export const QualityItem = styled(TouchableOpacity)`
+    padding: 5px 20px;
+    border-radius: 10px;
+    margin-right: 10px;
+    background-color: ${ Color.main };
+`
+
+export const QualityItemText = styled(Text)`
+    font-weight: 700;
+    color: white;
 `

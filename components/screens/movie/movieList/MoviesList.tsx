6bd821ca53757c5ee90f3e494ba2a2d2cbeed19f
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-27 14:30:17 +0100

    fixing the infinite scroll functionality

--- components/screens/movie/movieList/MoviesList.tsx
+++ components/screens/movie/movieList/MoviesList.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react'
 import { FlatList, Text } from 'react-native';
 import { Movie } from '../../../../types/movie';
 import Card from '../../../utils/card/Card'
 import Loading from '../../../utils/loading/Loading';
-import { Wrapper } from './styles/Styles'
 import { useInfiniteQuery } from "react-query";
 import axios from "axios";
 
@@ -13,17 +11,15 @@ export default function MoviesList({ navigation }: any) {
   const { isLoading, data, hasNextPage, fetchNextPage, isFetchingNextPage, isError } =
     useInfiniteQuery('movies', async ({ pageParam = 1 }) => {
       const result = await axios.get(`https://yts.mx/api/v2/list_movies.json?limit=15&page=${pageParam}`)
-      console.log(result.data.data.movies)
-      // console.log("page number: " + pageParam)
       return { 
         result, 
         nextPage: pageParam + 1, 
-        totalPages: Math.ceil(result.data.movie_count /15) 
+        totalPages: 80
       };
 
     } , {
       getNextPageParam: (lastPage, pages) => {
-         if (lastPage.nextPage < lastPage.totalPages) return lastPage.nextPage;
+        if (lastPage.nextPage < lastPage.totalPages) return lastPage.nextPage;
         return undefined;
       }
     });
@@ -48,17 +44,15 @@ export default function MoviesList({ navigation }: any) {
   if(isError) return <Text>Error Occured</Text>
 
   return (
-    // <Wrapper  >
-      <FlatList
-          contentContainerStyle={{ width: "100%", justifyContent: "center" }}
-          numColumns={3}
-          data={ data?.pages.map(page => page?.result.data?.data?.movies).flat() }
-          keyExtractor={ gameItemExtractorKey }
-          renderItem={ renderItem }
-          onEndReached={ loadMore }
-          onEndReachedThreshold={ 0.3 }
-          ListFooterComponent={ isFetchingNextPage ? <Loading size={ 50 } /> : null}
-        />
-    // </Wrapper>
+    <FlatList
+      contentContainerStyle={{ width: "100%", justifyContent: "center", paddingHorizontal: 10 }}
+      numColumns={3}
+      data={ data?.pages.map(page => page?.result.data?.data?.movies).flat() }
+      keyExtractor={ gameItemExtractorKey }
+      renderItem={ renderItem }
+      onEndReached={ loadMore }
+      onEndReachedThreshold={ 0.3 }
+      ListFooterComponent={ isFetchingNextPage ? <Loading size={ 50 } /> : null}
+    />
   )
 }

be813086f7750399f0b5c0ea64a7a8d1f81b8ac2
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-09 17:09:37 +0100

    get param passed from the search bar

--- components/screens/movie/searchMovie/SearchMovie.tsx
+++ components/screens/movie/searchMovie/SearchMovie.tsx
@@ -1,6 +1,10 @@
 import { View, Text } from 'react-native'
 
-export default function SearchMovie() {
+export default function SearchMovie({ route }: any) {
+
+    const title = route.params.title;
+    console.log(title)
+
   return (
     <View>
       <Text>SearchMovie</Text>

3cdb21ee378236458007f0696ba42110f3eb3b70
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-09 17:09:13 +0100

    change TextInput To controlled textInput

--- components/utils/navbar/Navbar.tsx
+++ components/utils/navbar/Navbar.tsx
@@ -1,18 +1,20 @@
 import { Field, GoHome, InputField, Logo, NavBarWrapper, Search, SearchAvatar } from "./styles/Styles";
 import { useNavigation } from '@react-navigation/native';
+import { useState } from "react";
 
 export default function Navbar() {
 
-  const navigation = useNavigation();
+  const navigation: any = useNavigation();
+  const [ title, setTitle ] = useState<string>('');
 
   return (
     <NavBarWrapper>
       <GoHome onPress={ () => navigation.canGoBack() && navigation.goBack() }>
-        <Logo source={require("../../../asset/logo.png")} />
+        <Logo source={ require("../../../asset/logo.png") } />
       </GoHome>   
       <Field>
-        <InputField placeholder="Tap to search"></InputField>
-        <Search onPress={ () => navigation.navigate("SearchMovie") }>
+        <InputField placeholder="Tap to search" value={ title } onChange={ (e) => setTitle(e.target.value) }></InputField>
+        <Search onPress={ () => navigation.navigate('SearchMovie', { title: title }) }>
           <SearchAvatar source={require("../../../asset/search.png")} />
         </Search>
       </Field>

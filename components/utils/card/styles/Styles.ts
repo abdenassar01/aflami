3242998a3f2e47e38e5e61898c6394b1c6a8d3ae
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-10 16:24:19 +0100

    fix cards styles

--- components/utils/card/styles/Styles.ts
+++ components/utils/card/styles/Styles.ts
@@ -2,15 +2,15 @@ import { Image, Text, View, Pressable, TouchableOpacity } from "react-native";
 import styled from "styled-components";
 
 export const CardWrapper = styled(Pressable)`
-    width: 30%;
-    margin-right: 5%;
+    width: 32%;
+    margin-right: 2%;
     margin-bottom: 5%;
 `
 
 export const Cover = styled(Image)`
     height: 170px;
     width: 100%;
-    border-radius: 10px;
+    border-radius: 5px;
 `
 
 export const Title = styled(Text)`

0bee489a086227e9141a6379abfce40b47f9783a
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-11 16:23:09 +0100

    fetching suggested movies and displaying them

--- components/utils/suggestions/Suggesetions.tsx
+++ components/utils/suggestions/Suggesetions.tsx
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native'
-import { MovieSuggestionProp } from '../../../types/movie'
-import { SuggestionsWrapper } from './styles/Styles'
+import { useNavigation } from '@react-navigation/native'
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import { Movie, MovieSuggestionProp } from '../../../types/movie'
+import Card from '../card/Card'
+import Loading from '../loading/Loading'
+import { Heading, SuggestionsWrapper } from './styles/Styles'
 
 export default function Suggesetions({ id }: MovieSuggestionProp) {
+
+  const navigation = useNavigation();
+
+  const { isLoading, error, data } = useQuery("getMovieSuggestion", async () => {
+    try{
+      const result = await axios.get(`https://yts.mx/api/v2/movie_suggestions.json?movie_id=${ id }`);
+      console.log(data?.data?.movies)
+      return result.data
+    }
+    catch(error){
+      return error
+    }
+  })
+
+  if(isLoading) return <Loading size={ 20 } />
+  if(error) return <Heading>can't load suggested movies. check network and try again</Heading>
+
   return (
-    <SuggestionsWrapper >
-      <Text>Suggesetions Movie Id: { id }</Text>
+    <SuggestionsWrapper 
+      contentContainerStyle={{ width: "100%", backgroundColor: "white", flexDirection: 'row' }}  
+    >
+      {
+        data?.data?.movies?.map((movie:Movie) => 
+          <Card key={ movie.id } movie={ movie } navigation={ navigation } />
+        )
+      }
     </SuggestionsWrapper>
   )
 }
--- components/utils/suggestions/styles/Styles.tsx
+++ components/utils/suggestions/styles/Styles.tsx
@@ -1,6 +1,10 @@
-import { ScrollView } from "react-native";
+import { ScrollView, Text } from "react-native";
 import styled from "styled-components";
 
 export const SuggestionsWrapper = styled(ScrollView)`
 
+`
+
+export const Heading = styled(Text)`
+    
 `
